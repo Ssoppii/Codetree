8d79b8696c1138ae10999bbc4a3882d16adf764f
Ssoppii <138455014+Ssoppii@users.noreply.github.com>
2025-04-10 21:51:49 +0900

    Title: 고대 문명 유적 탐사, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250410/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -0,0 +1,9 @@
+K, m = map(int, input().rstrip().split())
+board = []
+for _ in range(5):
+    tmp = list(map(int, input().rstrip().split()))
+    board.append(tmp)
+M = list(map(int, input().rstrip().split()))
+
+for k in range(K):
+    print(board[3,3])
